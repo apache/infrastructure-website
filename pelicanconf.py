dc2c66d62e3386c05b5fafb73bc6c4f149acb513
Greg Stein <gstein@gmail.com>
2017-03-30 02:15:27 -0500

    Move the TOC stuff, to make this look similar to devconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,11 +18,6 @@ PAGE_SAVE_AS = './{slug}.html'
 ARTICLE_SAVE_AS = 'news/{slug}.html'
 ARTICLE_URL = 'news/{slug}.html'
 
-# TOC Generator
-PLUGIN_PATHS = ['./theme/plugins']
-PLUGINS = ['toc']
-TOC_HEADERS = r"h[1-6]"
-
 # Sort news by date, descending, latest article first
 ARTICLE_ORDER_BY = 'reversed-date'
 
@@ -32,6 +27,11 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+# TOC Generator
+PLUGIN_PATHS = ['./theme/plugins']
+PLUGINS = ['toc']
+TOC_HEADERS = r"h[1-6]"
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
