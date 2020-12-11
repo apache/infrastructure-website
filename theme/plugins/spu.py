4fdebe28b3a47d749db9d84f3a61c13b61ad0ed6
Daniel Gruno <humbedooh@apache.org>
2020-12-11 17:30:25 +0100

    Create a collection of simple utils for pelican inline calls

---
+++ theme/plugins/spu.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This is a collection of simple in-page callable tools for pelican.
+To use a function, use the following syntax in your markdown or html:
+{{ spu:command_name("arg1", "arg2", "arg3") }}
+
+command_name must match a respective su_cmd_* command in python.
+"""
+import pelican.plugins.signals
+import pelican.contents
+import requests
+import re
+
+
+def su_cmd_fetch(args):
+    """Fetches an external URL and put the content where the call was made"""
+    url = args[0]
+    print("Fetching external resource " + url)
+    return requests.get(url).text
+
+
+def su_parse(instance: pelican.contents.Page):
+    my_functions = {k:v for k, v in globals().items() if callable(v) and k.startswith('su_cmd_')}
+    if instance._content is not None:
+        content = instance._content
+        for call in re.finditer(r"{{\s*spu:([_a-z]+)\(((?:(['\"]?)(.*?)\3(?:,\s*)?)*)\s*?\)\s*}}", content, flags=re.UNICODE):
+            errything = call.group(0)
+            cmd = call.group(1)
+            args = [x[1] for x in re.findall(r"(['\"]?)(.*?)\1(?:,\s*)?", call.group(2)) if x[1]]
+            fnc = "su_cmd_" + cmd
+            if fnc in my_functions:
+                retval = my_functions[fnc](args)
+                content = content.replace(errything, retval, 1)
+        if content != instance._content:
+            instance._content = content
+
+
+def register():
+    print("Simple Pelican Utils registered.")
+    pelican.plugins.signals.content_object_init.connect(su_parse)
